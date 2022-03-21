8fc2939be7fdf2c49664c74a75427f949682a6a5
这个名字没版权 <56252858+zhegemingzimeibanquan@users.noreply.github.com>
2022-03-21 11:12:53 +0800

    P

--- url_deal/svm_model.py
+++ url_deal/svm_model.py
@@ -1,6 +1,7 @@
+import joblib
 import pandas as pd
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 
 if __name__ == '__main__':
@@ -9,14 +10,14 @@ if __name__ == '__main__':
     # print(X.describe())
     y = data['label']
     train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.4, random_state=0)
-    clf = SVC()
-    clf.fit(train_x,train_y)
-    #joblib.dump(clf,'xss-svm-model.m')
-    y_pred=clf.predict(test_x)
+    clf = SVC(C=10, gamma=1, kernel='rbf')
+    clf.fit(train_x, train_y)
+    joblib.dump(clf, '../model/xss-svm-model.m')
+    y_pred = clf.predict(test_x)
     print(classification_report(test_y, y_pred))
 
-    # print(clf.get_params().keys())
-
+    print(clf.get_params())
+    #
     # params = {'C': [0.1, 1, 10], 'kernel': ['rbf', 'sigmoid', 'poly'],
     #           'gamma': [0.1, 1, 10]}
     # grid = GridSearchCV(estimator=clf, param_grid=params, n_jobs=8, verbose=2, cv=3)
